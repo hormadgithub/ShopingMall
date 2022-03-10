af6185aeb1f1d43078d1f3275349659268d5d6b2
hormadgithub <hormad.santi@gmail.com>
2022-03-10 16:36:02 +0700

    update today

---
+++ lib/states/create_account.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class CreateAccont extends StatefulWidget {
+  const CreateAccont({ Key? key }) : super(key: key);
+
+  @override
+  State<CreateAccont> createState() => _CreateAccontState();
+}
+
+class _CreateAccontState extends State<CreateAccont> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
